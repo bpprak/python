98dea68d9769f6e1ac5ce2a8f337958c4a359c72
Prakash Emmanuvel <prakbeno@MacBook-Pro.local>
2020-07-03 01:36:14 +0800

    add program

--- python_exercises/moveszeroes.py
+++ python_exercises/moveszeroes.py
@@ -3,7 +3,6 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         j = 0
         for num in nums:
-            print(nums)
             if(num != 0):
                 nums[j] = num
                 j += 1
@@ -17,5 +16,5 @@ class Solution:
 
 
 Object = Solution()
-x = [1,0,4,0,5]
+x = [1,0,4,0,5,9,3,9,0,2,0]
 print(Object.moveZeroes(x))
