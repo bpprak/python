0edbbfd7c94cef6509e9308167d8a9a83fb7704c
Prakash Emmanuvel <prakbeno@MacBook-Pro.local>
2020-07-21 17:52:10 +0800

    code update

---
+++ python_exercises/MinStack.py
@@ -0,0 +1,30 @@
+class MinStack:
+    def __init__(self):
+        self.st =  []
+    
+    def push(self, x:int) -> None:
+        curMin = self.getMin()
+        if curMin == None or curMin > x:
+            curMin = x
+        self.st.append((x, curMin))
+        
+    def pop(self) -> None:
+        self.st.pop()
+        
+    def top(self) ->  int:
+        return self.st[-1][0] if self.st else None
+        
+    def getMin(self) -> int:
+        return self.st[-1][1] if self.st else None
+    
+Object = MinStack()
+Object.push(1)
+Object.push(2)  
+print(Object.st)
+Object.push(0)
+print(Object.st)
+print(Object.top())
+print(Object.st)
+
+    
+        
